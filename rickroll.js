afa95dc9a0148e0e22ee0516162e267557ad4529
Nate Johnson <81366847+natejohnson05@users.noreply.github.com>
2022-03-22 22:46:40 -0400

    Create rickroll.js

---
+++ rickroll.js
@@ -0,0 +1,11 @@
+var mp3_url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+
+(new Audio(mp3_url)).play();
+
+alert('never gonna give you up');
+alert('never gonna let you down');
+alert('never gonna run around and desert you');
+alert('never gonna make you cry');
+alert('never gonna say goodbye");
+alert('never gonna tell a lie and hurt you');
+alert('lol get rickrolled');
