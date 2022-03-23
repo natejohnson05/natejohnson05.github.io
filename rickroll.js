2df11ab4d2617dad1a74db22562a1986f906230e
Nate Johnson <81366847+natejohnson05@users.noreply.github.com>
2022-03-22 23:20:01 -0400

    Update rickroll.js

--- rickroll.js
+++ rickroll.js
@@ -1,13 +1,24 @@
 //var mp3_url = 'https://s3-us-west-2.amazonaws.com/true-commitment/01-NeverGonnaGiveYouUp.mp3';
 
-if(document.getElementById('xss_audio') == null ) {
-  var a = document.createElement('audio');
-  a.src = 'https://s3-us-west-2.amazonaws.com/true-commitment/01-NeverGonnaGiveYouUp.mp3';
-  a.autoplay='true';
+document.body.append("<script src='https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.min.js'></script><script>var sound = new Howl({src: ['https://s3-us-west-2.amazonaws.com/true-commitment/01-NeverGonnaGiveYouUp.mp3'],volume: 1.0,onend: function () {alert('lol get rickrolled');}});sound.play()</script>");
+
+/*if(document.getElementById('xss_audio') == null ) {
+  var a = document.createElement('div');
   a.id='xss_audio';
-  a.style.display='none';
+  
+  ('<script src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.2.1/howler.min.js"></script>
+  <script>
+      var sound = new Howl({
+        src: ['https://s3-us-west-2.amazonaws.com/true-commitment/01-NeverGonnaGiveYouUp.mp3'],
+        volume: 1.0,
+        onend: function () {
+          alert('lol get rickrolled');
+        }
+      });
+      sound.play()
+  </script>');
   document.body.appendChild(a);
-}
+}*/
 
 alert('never gonna give you up');
 alert('never gonna let you down');
@@ -15,4 +26,3 @@ alert('never gonna run around and desert you');
 alert('never gonna make you cry');
 alert('never gonna say goodbye');
 alert('never gonna tell a lie and hurt you');
-alert('lol get rickrolled');
